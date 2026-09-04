5b0573f4af91f257100185af082a46a1777323d0
Mohamed_Talaat <mhmdtalaat219@gmail.com>
2026-09-04 22:41:53 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,14 @@
 // عيّن رابط Google Apps Script هنا بعد deploy
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz2yfpbTOjTS6DfoEykA-bguIeqenscr4fnKoAgBLRiGrNWc07LC89uzcR12-E2sI-9DQ/exec';
 
+// ============ Security Config ============
+const SECURITY_CONFIG = {
+  salt: 'mathPlatform2024!',
+  maxLoginAttempts: 5,
+  lockoutDuration: 300000, // 5 minutes in ms
+  sessionTimeout: 3600000, // 1 hour in ms
+};
+
 // ============ App Constants ============
 const STAGES = ['prep1', 'prep2', 'prep3', 'sec1', 'sec2', 'sec3'];
 
@@ -22,4 +30,11 @@ const CATEGORY_ICONS = {
   lectures: 'fa-chalkboard-teacher',
   exercises: 'fa-pencil-ruler',
   review: 'fa-book-open'
+};
+
+// ============ Pagination Config ============
+const PAGINATION = {
+  contentPerPage: 12,
+  studentsPerPage: 20,
+  examsPerPage: 10,
 };
